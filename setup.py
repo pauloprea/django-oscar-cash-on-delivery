283b914b144c2843788c6dc4bf43c3e94fedb84f
PaulO <atreidesone@gmail.com>
2014-11-27 16:20:48 +0000

    Bumped oscar version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='django-oscar-cash-on-delivery',
       license='BSD',
       packages=find_packages(exclude=['sandbox*', 'tests*']),
       include_package_data=True,
-      install_requires=['django-oscar>=0.6.4,<0.8'],
+      install_requires=['django-oscar>=0.6.4,<1.1'],
       # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=['Environment :: Web Environment',
                    'Framework :: Django',
